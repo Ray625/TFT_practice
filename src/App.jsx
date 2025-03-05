7ddefe862ddb8c7c2975df12eed138729513e74a
Ray625 <wdvgy32@gmail.com>
2025-03-05 20:40:29 +0800

    更新文字說明

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-end w-full h-screen bg-neutral-300/55 text-black">
+      <div className="flex flex-col items-center justify-end w-full h-fit min-h-screen pt-20 bg-neutral-300/55 text-black">
         <div className="w-fit mx-auto">
           <div className="relative">
             <Directions />
--- src/components/directions.jsx
+++ src/components/directions.jsx
@@ -2,12 +2,13 @@ const Directions = () => {
   return (
     <div className="absolute left-0 top-0 flex flex-col gap-2 ml-2 p-4 w-50 h-fit text-start text-text-white bg-reroll-bg border-2 border-reroll-border">
       <h3 className="text-2xl font-extrabold">TFT模擬器</h3>
-      <p className="text-lg">練習及熟悉陣容，金錢及等級可自行調整，金錢可透過單擊按鈕 +10元，也可直接點擊金錢並輸入數字。</p>
+      <p className="text-lg">練習及熟悉陣容，金錢及等級可自行調整。</p>
       <h3 className="text-2xl font-bold">操作說明</h3>
       <p className="text-lg">刷牌：D</p>
       <p className="text-lg">升級：F</p>
       <p className="text-lg">賣牌：將滑鼠指向棋子 + E</p>
       <p className="text-lg">放置棋子：將滑鼠指向棋子 + W</p>
+      <p className="text-sm">Tips：相同 1 星卡牌能合成 2 星，三隻 2 星將合成 3 星。</p>
     </div>
   );
 }

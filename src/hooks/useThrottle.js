d44014ae8d593a55225a605156b010f530dbf2f9
Ray625 <wdvgy32@gmail.com>
2025-03-10 16:33:43 +0800

    feat: 加入Throttle，避免長按D及F會過快觸發fnc

--- src/hooks/useThrottle.js
+++ src/hooks/useThrottle.js
@@ -1,15 +1,15 @@
-import { useRef } from "react";
+import { useRef, useCallback } from "react";
 
 const useThrottle = (fn, delay = 500) => {
   let lastCall = useRef(0);
 
-  return (...args) => {
+  return useCallback((...args) => {
     const now = new Date().getTime();
     if (now - lastCall.current < delay) return;
 
     lastCall.current = now;
     fn(...args);
-  };
+  }, [fn, delay])
 };
 
 export default useThrottle
